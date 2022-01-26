34644236416e6ce673552e1c451a4a1bf9cea450
Jacob Alder <j.cannonalder@gmail.com>
2022-01-26 16:42:08 -0500

    Created data import/clean file.

---
+++ 1_revenue-forecast/code/load.R
@@ -0,0 +1,45 @@
+################################################################################
+# F609, Dr. Denvil Duncan
+# Date: 1/26/22
+# Copyright: Jacob Alder, Indiana University
+# E-mail: alderjc@iu.edu
+# Dependencies: main.R
+# Outputs: 
+# Purpose: Revenue Forecasting Assignment for State of Indiana
+################################################################################
+loader <- function(sheet,range){
+  dt <-  read_excel("data/class-downloads/Data_for_Forecasting_HW.xlsx", 
+                    sheet = sheet,
+                    range = range,
+                    na = "NA")
+  setDT(dt)
+  return(dt)  
+}
+
+# Load & Clean files --------------------------------------------------------
+### General Tax Revenue
+tax <- loader("Data","A6:E203")
+tax = tax[!apply(is.na(tax) | tax == "", 1, all)]
+
+### Corporate Income Tax
+cit <- loader("CIT","C2:P17")
+# Rename variables
+cit = setnames(cit,1:12,c("July","August","September","October","November","December","January","Febuary","March","April","May","June"))
+cit = setnames(cit,13:14,c("YTD_Total","FY"))
+cit$FY[15]="FY2020"
+cit$FY=sub("FY","",cit$FY)
+cit.long=melt(cit,id.vars=c("FY","YTD_Total"),
+                  value.name="Revenue",
+                  variable.name="Month")
+
+# Format Date var
+cit.long$Day = 1
+cit.long$Date = as.Date(with(cit.long,paste(FY,Month,Day,sep="-")),"%Y-%B-%d")
+cit.long[,Day:=NULL]
+setorder(cit.long,FY,Month)
+
+# Reset CIT
+cit = cit.long
+rm(cit.long)
+
+rm(list=setdiff(ls(),c("cit","tax")))
---
+++ 1_revenue-forecast/code/main.R
@@ -0,0 +1,28 @@
+################################################################################
+# F609, Dr. Denvil Duncan
+# Date: 1/26/22
+# Copyright: Jacob Alder, Indiana University
+# E-mail: alderjc@iu.edu
+# Dependencies:
+# Outputs: 
+# Purpose: Revenue Forecasting Assignment for State of Indiana
+################################################################################
+# Setup -------------------------------------------------------------------
+file.dir = "/Users/jacobalder/OneDrive - Indiana University/f609-revtheory/revenue-theory"
+sub.dir = "1_revenue-forecast"
+if(!getwd() == file.path(file.dir,sub.dir)){setwd(file.path(file.dir,sub.dir))}
+
+# Load Packages
+if(!require("pacman")) install.packages("pacman")
+p_load(data.table,ggplot2,readxl)
+
+# p_load(here,haven,data.table,sandwich,lmtest,ggplot2,dplyr,tictoc,fixest,
+       # parallel,modelsummary,car,stargazer,gridExtra,collapse,ivreg,fastDummies,vtable,xtable)
+set.seed(121)
+options("digits" = 4)
+
+# Part A ------------------------------------------------------------------
+source("code/load.R")
+
+# Part B ------------------------------------------------------------------
+source("code/stats.R")
